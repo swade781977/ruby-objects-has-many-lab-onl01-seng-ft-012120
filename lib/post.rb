113f430e79d5ef6327eeaf9287e44d9b71a48d48
Chris Wade <swade781977@yahoo.com>
2020-02-04 18:45:10 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,6 +1,10 @@
 class Post
-  def initialize
+  @@all =[]
+  def initialize(title)
     @title = title
+    @author = author
   end
-  attr_accessor :title
+  attr_accessor :title :author
+  
+  def self.all
 end
