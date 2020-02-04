746cc4aaca870bb17a95f05f06d6f3080fd5b6d1
Chris Wade <swade781977@yahoo.com>
2020-02-04 19:41:09 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -8,8 +8,8 @@ class Artist
     song.artist = self
   end
   
-  def add_song_by_name(song)
-    song = Song.new(song)
+  def add_song_by_name(name)
+    song = Song.new(name)
     add_song(song)
   end
   
