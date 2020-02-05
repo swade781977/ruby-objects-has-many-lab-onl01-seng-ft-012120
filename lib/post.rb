26beac37e351d528b719bebe4fd5104647ed0ff4
Chris Wade <swade781977@yahoo.com>
2020-02-05 01:51:13 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -3,12 +3,13 @@ class Post
   def initialize(title)
     @title = title
     @author = author
+    @name = nil
     save
   end
   attr_accessor :title, :name, :author
   
   def author_name
-   author.name ||= nil
+   author.name
   end
   def save
     @@all << self
